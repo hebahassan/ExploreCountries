02731c72cc4035add9969963d2c500885fbb45c7
hebahassan <heba.hassan.elnashar@gmail.com>
2026-01-09 21:45:08 +0200

    Initial Commit

---
+++ ExploreCountries/ExploreCountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreCountriesApp.swift
+//  ExploreCountries
+//
+//  Created by Heba on 09/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ExploreCountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
